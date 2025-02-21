215573ae9d2aa7bb0b84609003dc559e62114f1e
Lisa <zhyganiuk.elizaveta@gmail.com>
2025-02-21 19:17:39 +0200

    added all files

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,14 @@
-FROM ubuntu:latest
-LABEL authors="STRIX"
+FROM python:3.11-slim
+LABEL maintainer="zhyganiuk.elizaveta@gmail.com"
 
-ENTRYPOINT ["top", "-b"]
+ENV PYTHONUNBUFFERED=1
+
+WORKDIR /app
+
+COPY requirements.txt requirements.txt
+
+RUN pip install -r requirements.txt
+
+COPY ./app .
+
+CMD ["python", "main.py"]
--- app/main.py
+++ app/main.py
@@ -1,6 +1,29 @@
+import os
+import requests
+
+
+API_KEY = os.getenv("API_KEY")
+CITY = os.getenv("CITY", "Paris")
+BASE_URL = "http://api.weatherapi.com/v1/current.json"
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    params = {
+        "key": API_KEY,
+        "q": CITY,
+        "aqi": "no"
+    }
+    response = requests.get(BASE_URL, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        temp_c = data["current"]["temp_c"]
+        condition = data["current"]["condition"]["text"]
+        print(f"Weather in {CITY}: {temp_c}°C, {condition}")
+    else:
+        print(f"Failed to get weather data: "
+              f"{response.status_code}, "
+              f"{response.text}")
 
 
 if __name__ == "__main__":
